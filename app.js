d762fd13a663c5dfa711bf932b33de9994d83fbf
Srijinesh Alanka <69688678+Srijinesh@users.noreply.github.com>
2022-07-09 22:58:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function submit(f,l){
+    alert("Thank you "+ f + l +" for signing up to StockZo");
+}
+
+var p = document.getElementById("portfolio");
+var ctx = p.getContext("2d");
+ctx.fillText("Standard text regarding portfolio");
